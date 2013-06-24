2ce1536decfa0ac1d6a6a75b1d263cd16197c0a1
yahiousun <yahiousun@gmail.com>
2013-06-24 17:59:48 +0800

    alpha 0.1

--- jquery.laconicpromised.js
+++ jquery.laconicpromised.js
@@ -31,39 +31,96 @@
 				
 				var navi = self.find(data.navi);
 				var contents = self.find(data.navi);
-				var total = navi.length;
 				var containerWidth = self.width();
 				var naviWidth = navi.eq(0).height();
 				var naviHeight = navi.eq(0).width();
-				var contentWidth = containerWidth-naviWidth*total;
+				var contentWidth = containerWidth-naviWidth*navi.length;
 				var offsetFix = (naviHeight-naviWidth)/2;
+				if(window.attachEvent){
+					offsetFix = 0;
+					navi.each(function(index, element) {
+						$(this).css({
+							'width': 30,
+							'height': 150
+						})
+					});
+				}
 				
 				function toggle(target, now){
 					
-					target.prevAll('dt').each(function(index){				
+					target.prevAll(data.navi).each(function(index){				
 						$(this).animate({
 							'left': -offsetFix+naviWidth*(now-index-1)
 						})
 					})
-					
-					target.prevAll('dd').each(function(index){
+					target.prevAll(data.content).each(function(index){
 						var _index = $(this).index('dd');
 						$(this).animate({
 							'left': naviWidth*(now-index)
 						})
 					})
-
-					target.nextAll('dt').each(function(index){
+					
+					target.animate({
+						'left': -offsetFix+naviWidth*now
+					}, function(){
+						$(this).addClass(data.active);
+					})
+					target.next(data.content).animate({
+						'left': naviWidth*(now+1)
+					})
+					
+					target.nextAll(data.navi).each(function(index){
 						$(this).animate({
 							'left': -offsetFix+naviWidth*(now+index+1)+contentWidth
 						})
 					})
-					target.next('dd').nextAll('dd').each(function(index){
+					target.next(data.content).nextAll(data.content).each(function(index){
 						$(this).animate({
 							'left': naviWidth*(now+index+2)+contentWidth
 						})
 					})
 					
+				}
+				
+				navi.eq(3).addClass(data.active)
+				
+				function newToggle(){
+					
+					var terms = self.find(data.navi);
+					var descs = self.find(data.content);
+					var now = terms.index($('.'+data.active));
+					alert(now)
+					// move target element
+					/*self.find('.'+data.active).animate({
+						'left': -offsetFix+naviWidth*now
+					}, data.duration, data.easing)*/
+					/*self.find('.'+data.active).next(data.content).animate({
+						'left': naviWidth*(descs.index($(this))+1)
+					}, data.duration, data.easing)*/
+					
+					// move prev element
+/*					self.find('.'+data.active).prevAll(data.navi).each(function(index){				
+						$(this).animate({
+							'left': -offsetFix+naviWidth*(terms.index($(this))-index-1)
+						}, data.duration, data.easing)
+					})
+					self.find('.'+data.active).prevAll(data.content).each(function(index){
+						var _index = $(this).index('dd');
+						$(this).animate({
+							'left': naviWidth*(descs.index($(this))-index+1)
+						}, data.duration, data.easing)
+					})
+					
+					self.find('.'+data.active).nextAll(data.navi).each(function(index){
+						$(this).animate({
+							'left': -offsetFix+naviWidth*(terms.index($(this))+index)+contentWidth
+						})
+					})
+					self.find('.'+data.active).next(data.content).nextAll(data.content).each(function(index){
+						$(this).animate({
+							'left': naviWidth*(descs.index($(this))+index+2)+contentWidth
+						})
+					})*/
 				}
 				
 				self.find(data.navi).each(function(index) {
@@ -83,11 +140,34 @@
 					})
 				})
 				
-				self.find(data.navi).each(function(now) {
-					$(this).bind('click.laconicpromised', function(){
-						toggle($(this), now);
+				self.find(data.navi).each(function() {
+					$(this).bind(data.trigger+'.laconicpromised', function(){
+						//toggle($(this), now);
+						//$(this).addClass(data.active).siblings(data.navi).removeClass(data.active);
+						newToggle();
 					})
 				});
+				
+				self.find(data.navi).each(function(){
+					var original, result = new String;
+					original = $(this).text();
+					original = original.split('');
+					
+					for (var i = 0; i < original.length; i++) {
+						result += "<span>"+(original[i])+"</span>"
+					}
+					
+					$(this).html(result);
+					$(this).find('span').each(function() {
+						if(/[^\x00-\xff]/g.test($(this).text())){
+							$(this).addClass('cn')
+						}
+					});
+				})
+				
+				if(data.autoplay){
+					
+				}
 				
 			})
 		},
@@ -134,7 +214,7 @@
 		gap: 0,
 		active: 'laconicpromised-active',
 		duration: 500,
-		autoplay: true,
+		autoplay: false,
 		interval: 3500,
 		trigger: 'click'
 	}
