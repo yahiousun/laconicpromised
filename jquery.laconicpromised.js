f328d8da63f388a3768de14a598c7ff97a6b8f20
yahiousun <yahiousun@gmail.com>
2013-06-25 17:31:01 +0800

    test new structure

--- jquery.laconicpromised.js
+++ jquery.laconicpromised.js
@@ -17,9 +17,9 @@
 				if(!data){ // if not initialized yet
 					self.data('laconicpromised', { // set data
 						container: settings.container,
-						navi: settings.navi,
+						key: settings.key,
 						content: settings.content,
-						gap: settings.gap,
+						//gap: settings.gap,
 						active: settings.active,
 						duration: settings.duration,
 						autoplay: settings.autoplay,
@@ -29,16 +29,16 @@
 					data = self.data('laconicpromised');
 				}
 				
-				var navi = self.find(data.navi);
-				var contents = self.find(data.navi);
+				var key = self.find(data.key);
+				var contents = self.find(data.key);
 				var containerWidth = self.width();
-				var naviWidth = navi.eq(0).height();
-				var naviHeight = navi.eq(0).width();
-				var contentWidth = containerWidth-naviWidth*navi.length;
-				var offsetFix = (naviHeight-naviWidth)/2;
-				if(window.attachEvent){
+				var keyWidth = key.eq(0).height();
+				var keyHeight = key.eq(0).width();
+				var contentWidth = containerWidth-keyWidth*key.length;
+				var offsetFix = (keyHeight-keyWidth)/2;
+				if(document.attachEvent && !document.addEventListener){ // if old IE
 					offsetFix = 0;
-					navi.each(function(index, element) {
+					key.each(function() {
 						$(this).css({
 							'width': 30,
 							'height': 150
@@ -46,87 +46,86 @@
 					});
 				}
 				
-				function toggle(target, now){
+				function toggle(target, current){
 					
-					target.prevAll(data.navi).each(function(index){				
+					target.prevAll(data.key).each(function(index){				
 						$(this).animate({
-							'left': -offsetFix+naviWidth*(now-index-1)
+							'left': -offsetFix+keyWidth*(current-index-1)
 						})
 					})
 					target.prevAll(data.content).each(function(index){
 						var _index = $(this).index('dd');
 						$(this).animate({
-							'left': naviWidth*(now-index)
+							'left': keyWidth*(current-index)
 						})
 					})
 					
 					target.animate({
-						'left': -offsetFix+naviWidth*now
+						'left': -offsetFix+keyWidth*current
 					}, function(){
 						$(this).addClass(data.active);
 					})
 					target.next(data.content).animate({
-						'left': naviWidth*(now+1)
+						'left': keyWidth*(current+1)
 					})
 					
-					target.nextAll(data.navi).each(function(index){
+					target.nextAll(data.key).each(function(index){
 						$(this).animate({
-							'left': -offsetFix+naviWidth*(now+index+1)+contentWidth
+							'left': -offsetFix+keyWidth*(current+index+1)+contentWidth
 						})
 					})
 					target.next(data.content).nextAll(data.content).each(function(index){
 						$(this).animate({
-							'left': naviWidth*(now+index+2)+contentWidth
+							'left': keyWidth*(current+index+2)+contentWidth
 						})
 					})
 					
 				}
 				
-				navi.eq(3).addClass(data.active)
+				key.eq(3).addClass(data.active)
 				
 				function newToggle(){
 					
-					var terms = self.find(data.navi);
-					var descs = self.find(data.content);
-					var now = terms.index($('.'+data.active));
-					alert(now)
+					var keys = self.find(data.key);
+					var current = keys.index($('.'+data.active));
+					
 					// move target element
-					/*self.find('.'+data.active).animate({
-						'left': -offsetFix+naviWidth*now
-					}, data.duration, data.easing)*/
-					/*self.find('.'+data.active).next(data.content).animate({
-						'left': naviWidth*(descs.index($(this))+1)
-					}, data.duration, data.easing)*/
+					self.find('.'+data.active).animate({
+						'left': -offsetFix+keyWidth*current
+					}, data.duration, data.easing)
+					self.find('.'+data.active).next(data.content).animate({
+						'left': keyWidth*(current+1)
+					}, data.duration, data.easing)
 					
 					// move prev element
-/*					self.find('.'+data.active).prevAll(data.navi).each(function(index){				
+					self.find('.'+data.active).prevAll(data.key).each(function(index){				
 						$(this).animate({
-							'left': -offsetFix+naviWidth*(terms.index($(this))-index-1)
+							'left': -offsetFix+keyWidth*(current-index-1)
 						}, data.duration, data.easing)
 					})
 					self.find('.'+data.active).prevAll(data.content).each(function(index){
 						var _index = $(this).index('dd');
 						$(this).animate({
-							'left': naviWidth*(descs.index($(this))-index+1)
+							'left': keyWidth*(current-index)
 						}, data.duration, data.easing)
 					})
 					
-					self.find('.'+data.active).nextAll(data.navi).each(function(index){
+					self.find('.'+data.active).nextAll(data.key).each(function(index){
 						$(this).animate({
-							'left': -offsetFix+naviWidth*(terms.index($(this))+index)+contentWidth
+							'left': -offsetFix+keyWidth*(current+index+1)+contentWidth
 						})
 					})
 					self.find('.'+data.active).next(data.content).nextAll(data.content).each(function(index){
 						$(this).animate({
-							'left': naviWidth*(descs.index($(this))+index+2)+contentWidth
+							'left': keyWidth*(current+index+2)+contentWidth
 						})
-					})*/
+					})
 				}
 				
-				self.find(data.navi).each(function(index) {
+				self.find(data.key).each(function(index) {
 					$(this).css({
 						'top': offsetFix,
-						'left': -offsetFix+naviWidth*index,
+						'left': -offsetFix+keyWidth*index,
 						'z-index': index
 					})
 				});
@@ -135,20 +134,20 @@
 					$(this).css({
 						'width': contentWidth,
 						'top': 0,
-						'left': naviWidth*(index+1),
+						'left': keyWidth*(index+1),
 						'z-index': index
 					})
 				})
 				
-				self.find(data.navi).each(function() {
+				self.find(data.key).each(function() {
 					$(this).bind(data.trigger+'.laconicpromised', function(){
-						//toggle($(this), now);
-						//$(this).addClass(data.active).siblings(data.navi).removeClass(data.active);
+						//toggle($(this), current);
+						$(this).addClass(data.active).siblings(data.key).removeClass(data.active);
 						newToggle();
 					})
 				});
 				
-				self.find(data.navi).each(function(){
+				self.find(data.key).each(function(){
 					var original, result = new String;
 					original = $(this).text();
 					original = original.split('');
@@ -209,10 +208,10 @@
 	// default settings
 	$.fn.laconicpromised.defaults = {
 		container: '',
-		navi: 'dt',
+		key: 'dt',
 		content: 'dd',
-		gap: 0,
-		active: 'laconicpromised-active',
+		//gap: 0,
+		active: 'active',
 		duration: 500,
 		autoplay: false,
 		interval: 3500,
